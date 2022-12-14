347ad9911881fd59bc7e7c17f4ffe81098b23653
Rafael de Oliveira (Hellz) <rafael.oliveirasylva@gmail.com>
2022-12-14 01:05:02 -0300

    chore: swagger user routes implementation

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -9,8 +9,8 @@ const router = Router();
 
 router.get(
   '/users',
-  // verifyToken,
-  // isAdmin,
+  verifyToken,
+  isAdmin,
   UserController.getAllUsers,
   (_req, _res) => {
     // #swagger.tags = ['User']
@@ -22,9 +22,41 @@ router.get(
       type: 'string',
       schema: { $ref: "#/definitions/VerifyUser" }
     }*/
+    /* #swagger.parameters['page'] = {
+      in: 'query',
+      description: 'Número da página.',
+      required: false,
+      type: 'number',
+    }
+    #swagger.parameters['limit'] = {
+      in: 'query',
+      description: 'Limite de usuários por página.',
+      required: false,
+      type: 'number',
+    }
     /* #swagger.responses[200] = {
     description: 'Listar todos os usuários no sistema.',
     schema: { $ref: "#/definitions/User" }
+  }*/
+    /* #swagger.responses[400] = {
+    description: 'Requisição inválida.',
+    schema: { $ref: "#/definitions/BadRequest" }
+    }*/
+    /* #swagger.responses[401] = {
+    description: 'Acesso não autorizado.',
+    schema: { $ref: "#/definitions/Unauthorized" }
+    }*/
+    /* #swagger.responses[403] = {
+    description: 'Acesso negado.',
+    schema: { $ref: "#/definitions/Forbidden" }
+    }*/
+    /* #swagger.responses[404] = {
+    description: 'Usuário não encontrado.',
+    schema: { $ref: "#/definitions/NotFound" }
+    }*/
+    /* #swagger.responses[500] = {
+    description: 'Erro interno do servidor.',
+    schema: { $ref: "#/definitions/InternalServerError" }
   }*/
   }
 );
