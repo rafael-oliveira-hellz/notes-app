ae827b2167772aad89d2244eb1677ecdaf58b957
Rafael de Oliveira (Hellz) <rafael.oliveirasylva@gmail.com>
2022-12-24 13:18:47 -0300

    fix: error handlers

--- src/controllers/note.controller.ts
+++ src/controllers/note.controller.ts
@@ -6,6 +6,7 @@ import { paginate } from '../middlewares/Pagination';
 import { getUserByToken, getUserToken } from '../middlewares/TokenControl';
 import { IUser } from '../models/interfaces/user';
 import Note from '../models/Note';
+import User from '../models/User';
 
 class NoteController {
   // [TO TEST] Get all notes (admin only)
@@ -23,7 +24,7 @@ class NoteController {
     }
 
     response.data = response.data.map(async (note: any) => {
-      const user = await getUserByToken(note.assignee);
+      const user = await User.findById(note.assignee);
       return {
         id: note?.id,
         title: note?.title,
