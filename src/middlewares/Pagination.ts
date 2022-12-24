09356e486d26604047015f8a45ae8ea76be7fb29
Rafael de Oliveira (Hellz) <rafael.oliveirasylva@gmail.com>
2022-12-24 11:32:19 -0300

    fix: error handlers

--- src/middlewares/Pagination.ts
+++ src/middlewares/Pagination.ts
@@ -1,5 +1,5 @@
 import { Request } from 'express';
-import { ReasonPhrases, StatusCodes } from 'http-status-codes';
+import { StatusCodes } from 'http-status-codes';
 import logger from '../config/winston-logger';
 
 const paginate = async (model: any, req: Request, res: any) => {
@@ -18,85 +18,77 @@ const paginate = async (model: any, req: Request, res: any) => {
   };
 
   try {
-    try {
-      result.totalDocuments = await model.countDocuments().exec();
-    } catch (error) {
-      logger.error(error);
-      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-        message: 'Error counting documents',
-        error,
-      });
-    }
-
-    try {
-      result.totalPages = Math.ceil(result.totalDocuments / limit);
-    } catch (error) {
-      logger.error(error);
-      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-        message: 'Error counting pages',
-        error,
-      });
-    }
-
-    if (endIndex < (await model.countDocuments().exec())) {
-      result.next = {
-        page: page + 1,
-        limit,
-      };
-    }
-
-    if (startIndex > 0) {
-      result.previous = {
-        page: page - 1,
-        limit,
-      };
-    }
-
-    try {
-      result.data = await model.find().limit(limit).skip(startIndex).exec();
-    } catch (error) {
-      logger.error(error);
-      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-        message: 'Error finding documents',
-        error,
-      });
-    }
-
-    result.current = {
-      page,
-      limit,
-    };
-
-    res.paginatedResult = result;
-
-    if (result.data.length === 0) {
-      logger.error('Nenhum dado encontrado com os parâmetros fornecidos', {
-        success: false,
-        statusCode: StatusCodes.NOT_FOUND,
-        result,
-      });
-
-      return res.status(StatusCodes.NOT_FOUND).json({
-        message: 'Nenhum dado encontrado com os parâmetros fornecidos',
-      });
-    }
-
-    logger.debug('Dados encontrados com sucesso.', {
-      success: true,
-      statusCode: StatusCodes.OK,
-      label: model.collection.collectionName,
-      method: 'GET',
-      result,
-    });
-
-    return result;
+    result.totalDocuments = await model.countDocuments().exec();
   } catch (error) {
     logger.error(error);
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      message: ReasonPhrases.INTERNAL_SERVER_ERROR,
-      body: error,
+      message: 'Error counting documents',
+      error,
     });
   }
+
+  try {
+    result.totalPages = Math.ceil(result.totalDocuments / limit);
+  } catch (error) {
+    logger.error(error);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: 'Error counting pages',
+      error,
+    });
+  }
+
+  if (endIndex < (await model.countDocuments().exec())) {
+    result.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+
+  if (startIndex > 0) {
+    result.previous = {
+      page: page - 1,
+      limit,
+    };
+  }
+
+  try {
+    result.data = await model.find().limit(limit).skip(startIndex).exec();
+  } catch (error) {
+    logger.error(error);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: 'Error finding documents',
+      error,
+    });
+  }
+
+  result.current = {
+    page,
+    limit,
+  };
+
+  res.paginatedResult = result;
+
+  if (result.data.length === 0) {
+    logger.error('Nenhum dado encontrado com os parâmetros fornecidos', {
+      success: false,
+      statusCode: StatusCodes.NOT_FOUND,
+      result,
+    });
+
+    return res.status(StatusCodes.NOT_FOUND).json({
+      message: 'Nenhum dado encontrado com os parâmetros fornecidos',
+    });
+  }
+
+  logger.debug('Dados encontrados com sucesso.', {
+    success: true,
+    statusCode: StatusCodes.OK,
+    label: model.collection.collectionName,
+    method: 'GET',
+    result,
+  });
+
+  return result;
 };
 
 export { paginate };
